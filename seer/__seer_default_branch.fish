0bed1680b0fc9be13acca4545d3895d125394cfb
Kiran Capoor <kiran.capoor94@gmail.com>
2026-03-23 15:39:25 +0530

    chore: remove old ritual bootstrap files

--- seer/__seer_default_branch.fish
+++
@@ -1,3 +0,0 @@
-function __seer_default_branch
-    git symbolic-ref --quiet refs/remotes/origin/HEAD 2>/dev/null | string replace "refs/remotes/origin/" ""
-end
--- seer/__seer_repo_root.fish
+++
@@ -1,3 +0,0 @@
-function __seer_repo_root
-    git rev-parse --show-toplevel 2>/dev/null
-end
